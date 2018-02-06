f1bc6a25d8b6cda14e47af4912d32b403fb680a9
Benjamin NGOUM <benjamin.ngoum@gmail.com>
2018-02-06 15:49:15 +0100

    Ajout des repères sur le Go Ban

--- views/template.php
+++ views/template.php
@@ -9,92 +9,164 @@
     <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
-<table>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+<div class="wrapper-goban">
+    <ul class="repere-lettres">
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+        <li>F</li>
+        <li>G</li>
+        <li>H</li>
+        <li>I</li>
+    </ul>
 
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+    <ul class="repere-lettres">
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+        <li>F</li>
+        <li>G</li>
+        <li>H</li>
+        <li>I</li>
+    </ul>
 
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+    <ul class="repere-chiffres">
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+    </ul>
 
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+    <ul class="repere-chiffres">
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+    </ul>
+    <table>
+            <tr>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+            </tr>
 
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+            <tr>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+            </tr>
 
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+            <tr>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+                <td><span class="pion"></span></td>
+            </tr>
 
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>
-    
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
     <script src="../public/js/jquery-3.3.1.min.js"></script>
     <script src="../public/js/script.js"></script>
 </body>
